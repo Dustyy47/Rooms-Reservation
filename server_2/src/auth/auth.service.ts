6a746b7545e8424756da8efef9bb648295a7fa41
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-04-06 22:07:57 +0300

    Installed bcrypt, add users sign up

--- server_2/src/auth/auth.service.ts
+++ server_2/src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from './../prisma/prisma.service';
 import { SignUpStudentDTO, SignUpTeacherDTO } from './dto';
 
@@ -6,12 +7,46 @@ import { SignUpStudentDTO, SignUpTeacherDTO } from './dto';
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signUpStudent(dto: SignUpStudentDTO) {
-    return 'signed up Student!';
+  async signUpStudent(dto: SignUpStudentDTO) {
+    try {
+      const hashedPassword = await bcrypt.hash(dto.password, 10);
+      const student = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hashedPassword,
+          fullname: dto.fullname,
+          course: dto.course,
+          type: 'STUDENT',
+        },
+      });
+      return student;
+    } catch (e) {
+      if (e.code === 'P2002') {
+        throw new ConflictException('Credentials taken');
+      }
+      throw e;
+    }
   }
 
-  signUpTeacher(dto: SignUpTeacherDTO) {
-    return 'signed up Teacher!';
+  async signUpTeacher(dto: SignUpTeacherDTO) {
+    try {
+      const hashedPassword = await bcrypt.hash(dto.password, 10);
+      const teacher = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hashedPassword,
+          fullname: dto.fullname,
+          speciality: dto.speciality,
+          type: 'TEACHER',
+        },
+      });
+      return teacher;
+    } catch (e) {
+      if (e.code === 'P2002') {
+        throw new ConflictException('Credentials taken');
+      }
+      throw e;
+    }
   }
 
   signIn() {
