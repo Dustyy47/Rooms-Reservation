fd08c61b07f2307e7afdd0c60aaefbf837ef35c5
Dmitry <109687670+Dustyy47@users.noreply.github.com>
2023-04-13 22:16:16 +0300

    Add cors support

--- server_2/src/main.ts
+++ server_2/src/main.ts
@@ -7,6 +7,7 @@ const PORT = 5000
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.enableCors()
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
